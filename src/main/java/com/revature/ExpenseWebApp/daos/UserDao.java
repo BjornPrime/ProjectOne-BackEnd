ce3ceade858e3a2a166fd1b3b97953a196958020
BjornPrime <bjorn291@gmail.com>
2019-02-14 18:02:38 -0500

    end of day 2/14

--- src/main/java/com/revature/ExpenseWebApp/daos/UserDao.java
+++ src/main/java/com/revature/ExpenseWebApp/daos/UserDao.java
@@ -1,6 +1,13 @@
 package com.revature.ExpenseWebApp.daos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.revature.ExpenseWebApp.models.User;
+
 public class UserDao {
+	
+	Connection conn = DriverManager.getConnection()
 	
 	public User insertUser(User user) {
 		
